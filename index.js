6e2e21d56c38abd1851243186b5194c12fc7aeaf
John K. Speck <speckworks@gmail.com>
2020-06-23 12:34:40 +0000

    test cases added.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 
   //Definition for singly-linked list.
  function ListNode(val, next) {
-      this.val = (val===undefined ? 0 : val)
-      this.next = (next===undefined ? null : next)
+      this.val = (val === undefined ? 0 : val)
+      this.next = (next === undefined ? null : next)
  }
 
 const addTwoNumbers = (l1, l2) => {
   let carry = 0, sum = 0, head = new ListNode(0), now = head, a = l1, b = l2;
+
   while (a !== null || b !== null) {
     sum = (a ? a.val : 0) + (b ? b.val : 0) + carry;
     carry = Math.floor(sum / 10);
@@ -19,4 +20,6 @@ const addTwoNumbers = (l1, l2) => {
   return head.next;
 };
 
-addTwoNumbers([1,2,3], [8,9,1])
+addTwoNumbers([1,2,3], [8,9,1]);
+
+//this returns Nan for val.. not sure why.  I tried to iterated with a simple for loop, didn't change anything. 
